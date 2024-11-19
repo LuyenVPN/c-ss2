93edc70d12e4f0014edc204e06998675da80139d
Đặng Văn Luyện <133982516+LuyenVPN@users.noreply.github.com>
2024-11-19 15:06:30 +0700

    b1

---
+++ b1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a=10;//Khai bao va khai tao bien a la kieu du lieu so nguyen, gia tri la 10
+	printf("a=%d\n",a);	
+	char b='B';//Khai bao va khai tao bien b la kieu du lieu ki tu, gia tri la B 
+	printf("b=%c\n",b);
+	float c=3.14;//Khai bao va khai tao bien b la kieu du lieu so thuc, gia tri la 3.14
+	printf("c=%f\n",c);
+	double d=3.1444444;//Khai bao va khai tao bien b la kieu du lieu so thuc, gia tri la 3.144444 
+	printf("d=%lf\n",d);
+	return 0;
+	
+}
